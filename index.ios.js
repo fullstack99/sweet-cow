9af0cf253e42e0e127e9b7ab15ff8b66b631db82
fullstack99 <success1989127@gmail.com>
2017-01-27 22:26:18 -0700

    First commit, building boiler plate for sweet cow app. Creating starting modules for iOS.

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+// Only iOS code goes here!!!
+
+// Import library to help create a Component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a component
+const App = () => (
+    <Header headerText={'Sweet Cow'} />
+);
+
+// Render to device
+AppRegistry.registerComponent('sweetcow', () => App);
